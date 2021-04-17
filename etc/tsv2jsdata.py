5e3f3f8b8656164663ab76be660654ea7d60849c
TSUYUKI kohei <kotsuyuki@gmail.com>
2021-04-17 11:11:36 +0900

    パールスの勝率を表示するところまで実装

---
+++ etc/tsv2jsdata.py
@@ -0,0 +1,5 @@
+import csv
+import sys
+
+if __name__ == '__main__':
+    print("Hello")
--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,42 @@
 import { Container } from "@material-ui/core";
-import { Line, LineChart } from "recharts";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { WIN_RATE } from "./winrate";
 
 function App() {
-  const data = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
   return (
     <div>
       <Container maxWidth="md">
         <h1>Baystars(2021) vs. Pearls(1958)</h1>
-        <LineChart width={400} height={400} data={data}>
-          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <LineChart
+          width={960}
+          height={300}
+          data={WIN_RATE}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="pearls"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
         </LineChart>
       </Container>
     </div>
