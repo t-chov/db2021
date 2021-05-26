ec2e5c1fbdcb8d8149cf4693e069fb8316db005d
Kohei Tsuyuki <kotsuyuki@gmail.com>
2021-05-26 21:05:37 +0900

    2021-05-26 の試合結果を反映

--- src/winrate.ts
+++ src/winrate.ts
@@ -54,6 +54,7 @@ export const WIN_RATE: WinrateDatapoint[] = [
   { name: 'Game 46', pearls: 0.227, baystars: 0.3, unions: 0.326 },
   { name: 'Game 47', pearls: 0.222, baystars: 0.293, unions: 0.34 },
   { name: 'Game 48', pearls: 0.217, baystars: 0.31, unions: 0.333 },
+  { name: 'Game 49', pearls: 0.213, baystars: 0.302, unions: 0.327 },
 ]
 
 export function invertDatapoints(datapoints: WinrateDatapoint[]): WinrateDatapoint[] {
