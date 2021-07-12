453f9e6afb3215cf7a5beaf271f0c3ceddfe25bc
TSUYUKI kohei <kotsuyuki@gmail.com>
2021-07-12 21:15:24 +0900

    文言を追加

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,7 @@ function App() {
           label={'inverted mode'}
         />
         <p>横浜DeNAベイスターズ(2021), 近鉄パールス(1958), トンボユニオンズ(1955) の勝率を試合数ベースで比較します。</p>
+        <p>2021-07-12追記: 勝率4割ほどになり、比較の必要性が薄れてきたので更新が遅くなります。</p>
         <TwitterShareButton url={'https://t-chov.github.io/db2021/'} />
         <RateGraph datapoints={datapoints} />
       </Container>
